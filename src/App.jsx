a7a22af9dac16ccb02d9602b8d71573ae65584c6
chuhoon <khchu1220@naver.com>
2022-06-28 02:13:02 +0900

    addProduct 브렌치 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset"
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
-`
+`;
 
 function App() {
     return (
         <React.Fragment>
-            <GlobalStyle/>
+            <GlobalStyle />
             <div className="App">
                 <h1>App</h1>
             </div>
