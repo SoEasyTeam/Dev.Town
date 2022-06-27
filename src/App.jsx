07cd2d356b7bd86f78bb43c66703d3f824b6b17a
chuhoon <khchu1220@naver.com>
2022-06-28 01:40:59 +0900

    초기세팅 수정 assets, reset 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset"
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+`
+
 function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+    return (
+        <React.Fragment>
+            <GlobalStyle/>
+            <div className="App">
+                <h1>App</h1>
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default App;
