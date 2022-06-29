00aa7cb6d3b2861aab5694d13903221925132ae0
skylar121 <myway01212@naver.com>
2022-06-29 19:58:02 +0900

    Feat & Style: 프로필 사진, 업로드 버튼 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { DefaultProfileImg, ImgUploadBtn, ImgGrayBtn } from './components/common/ProfileButtons';
 import Splash from './components/splash/Splash';
 
 function App() {
     return (
         <>
             <Splash></Splash>
+            <div style={{width: '50px'}}>
+                <DefaultProfileImg />
+                <ImgUploadBtn />
+                <ImgGrayBtn />
+            </div>
+            
         </>
     );
 }
