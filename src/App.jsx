b3bfe7ea41c3f24fc1bbe6efcf4d824a9acd3179
skylar121 <myway01212@naver.com>
2022-06-30 00:37:13 +0900

    Feat & Style: 버튼 컴포넌트 추가 (다음, 저장, 팔로우)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import { DefaultProfileImg, ImgUploadBtn, ImgGrayBtn } from './components/common/ProfileButtons';
-import Splash from './components/splash/Splash';
 
 function App() {
     return (
         <>
-            <Splash></Splash>
-            <div style={{width: '50px'}}>
-                <DefaultProfileImg />
-                <ImgUploadBtn />
-                <ImgGrayBtn />
-            </div>
-            
         </>
     );
 }
---
+++ src/components/common/Buttons.jsx
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+const LBtns = styled.button`
+    width: 322px;
+    height: 44px;
+    ${({isActive}) => {
+        return isActive === true? `background-color: var(--main-color)` : `background-color: var(--main-disabled-color)`}
+    };
+    border-radius: 44px;
+    color: var(--subtitle-text);
+`
+
+const MBtns = styled.button`
+    width: 120px;
+    height: 34px;
+    ${({isFollowed}) => {
+        return isFollowed === true? `background-color: var(--bg-color)` : `background-color: var(--main-color)`}
+    };
+    border: 1px solid #DBDBDB;
+    border-radius: 30px;
+    color: var(--subtitle-text);
+`
+
+const MsBtns = styled.button`
+    width: 90px;
+    height: 32px;
+    ${({isActive}) => {
+        return isActive === true? `background-color: var(--main-color)` : `background-color: var(--main-disabled-color)`}
+    };
+    border-radius: 32px;
+    color: var(--subtitle-text);
+`
+
+const SBtns = styled.button`
+    width: 65px;
+    height: 28px;
+    ${({isFollowed}) => {
+        return isFollowed === true? `background-color: var(--bg-color)` : `background-color: var(--main-color)`}
+    };
+    border: 1px solid #DBDBDB;
+    border-radius: 30px;
+    color: var(--subtitle-text);
+`
+
+function LBtn({isActive}) {
+    return (
+        <LBtns type="button" isActive={isActive}>다음</LBtns>
+    )
+}
+
+function MBtn({isFollowed}) {
+    return (
+        <MBtns type="button" isFollowed={isFollowed}>
+            {isFollowed === true? '언팔로우' : '팔로우'}
+        </MBtns>
+    )
+}
+
+function MsBtn({isActive}) {
+    return (
+        <MsBtns type="button" isActive={isActive}>저장</MsBtns>
+    )
+}
+
+function SBtn({isFollowed}) {
+    return (
+        <SBtns type="button" isFollowed={isFollowed}>
+            {isFollowed === true? '언팔로우' : '팔로우'}
+        </SBtns>
+    )
+}
+
+export { LBtn, MBtn, MsBtn, SBtn };
