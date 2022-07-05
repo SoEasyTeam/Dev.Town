5bda1b15c95bcca6c92914245dab5c9dd4656b72
chuhoon <khchu1220@naver.com>
2022-07-06 01:21:35 +0900

    App 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import AddProductPage from './pages/AddProductPage'
+import HomePage from './pages/HomePage'
 
 function App() {
     return (
         <>
-            <AddProductPage />
+            <HomePage />
         </>
     );
 }
