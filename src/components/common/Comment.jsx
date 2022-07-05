c6f1265e7d6528c5229d0da38f5caebb1f26755d
Dahee Jo <happyj6528@gmail.com>
2022-07-05 21:19:28 +0900

    Style: border 색상 변경

--- src/components/common/Comment.jsx
+++ src/components/common/Comment.jsx
@@ -5,7 +5,7 @@ const CommentBox = styled.div`
     width: 100%;
     height: 61px;
     display: flex;
-    border: 1px solid black;
+    border: 1px solid  --border-gray;
 `;
 
 const ProfileImgBox = styled.div`
