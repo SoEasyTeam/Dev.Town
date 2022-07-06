1e2806707353bb8c9bdd78d020ed741004152fe3
Dahee Jo <happyj6528@gmail.com>
2022-07-06 14:22:52 +0900

    임시 완성

--- src/pages/Error404Page.jsx
+++ src/pages/Error404Page.jsx
@@ -1,12 +1,28 @@
 import ErrorIcon from '../assets/error404-icon.png'
 import styled from 'styled-components';
-// const Error404Img = styled.img`
+import { LBtn, MBtn, MsBtn, SBtn, MBtns } from '../components/common/Buttons'
 
-// `
+const ErrorPageBox=styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const ErrorMessage=styled.p`
+    margin:20px 0;
+    font-size: 14px;
+    color: var(--subtitle-text);
+`
 function Error404Page(){
-    <>
-    <img src={ErrorIcon} alt='404에러' style={{width:100, height:100}}/>
-    </>
+    return (
+    <ErrorPageBox>
+        <img src={ErrorIcon} alt='404에러' style={{width:158, height:158}}/>
+        <ErrorMessage>페이지를 찾을 수 없습니다 :(</ErrorMessage>
+        <MBtns/>
+    </ErrorPageBox>
+    )
 }
 
 export default Error404Page;
