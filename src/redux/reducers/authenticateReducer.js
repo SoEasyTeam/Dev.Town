afc825eeb63ca9775bec013431d85c5e58acfde9
chuhoon <khchu1220@naver.com>
2022-07-18 12:46:21 +0900

    로그인 리듀서 값 수정

--- src/redux/reducers/authenticateReducer.js
+++ src/redux/reducers/authenticateReducer.js
@@ -1,18 +1,27 @@
 let initialState = {
     email: '',
-    password: '',
+    id: '',
+    username: '',
+    accountname: '',
+    image: '',
+    token: '',
     authenticate: false,
 };
 
 function authenticateReducer(state = initialState, action) {
     let { type, payload } = action;
+    console.log(action);
     switch (type) {
         case 'LOGIN_SUCCESS':
             console.log('login success reducer');
             return {
                 ...state,
+                id: payload._id,
+                username: payload.username,
                 email: payload.email,
-                password: payload.password,
+                accountname: payload.accountname,
+                image: payload.image,
+                token: payload.token,
                 authenticate: true,
             };
         default:
