062bb48485886967154f6c79799328b0b475765a
daheejo <happyj6528@gmail.com>
2022-07-18 13:21:26 +0900

    reducer 작업중

--- src/redux/reducers/searchReducer.js
+++ src/redux/reducers/searchReducer.js
@@ -3,5 +3,16 @@ let initialState={
 }
 
 function searchReducer(state=initialState, action){
-    
+    let {type, payload} = action
+    switch (type) {
+        case 'SEARCH_USER':
+            return {
+                ...state,
+
+            }
+        default:
+            return {...state}
+    }
 }
+
+export default searchReducer
