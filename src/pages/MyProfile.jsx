02134badeaa8148f22e5b1c38dddf6d828b7bc4a
Dayhun <lanna05@naver.com>
2022-07-18 10:55:40 +0900

    Feat: myprofile post기능 생성

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import { TopBasicNav } from '../components/common/TopNav';
 import TabMenu from '../components/common/TabMenu';
-import HomeImgPost from '../components/common/HomeImgPost';
-import IconPostListOn from '../assets/icon/icon-post-list-on.png';
-import IconPostAlbumOff from '../assets/icon/icon-post-album-off.png';
 import UserProfile from '../features/UserProfile';
 import UserProduct from '../features/UserProduct';
+import UserPost from '../features/UserPost';
 
 const ProfileSection = styled.section`
     display: flex;
@@ -20,47 +17,6 @@ const ProfileSection = styled.section`
     min-height: 892px;
 `
 
-const PostLink = styled(Link)`
-
-`
-
-const PostShowBtns = styled.button`
-    width: 26px;
-    height: 26px;
-    margin-right: 16px;
-`
-
-const PostArea = styled.article`
-    width: 100vw;
-    border-top: 0.5px solid #DBDBDB;
-    background: #FFFFFF;
-    .postBtnImg {
-        width: 26px;
-    }
-    .postAreaTop {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-        border-bottom: 0.5px solid #DBDBDB;
-        height: 43px
-    }
-`
-const PostAreaListUl = styled.ul`
-    list-style: none;
-    li {
-        margin: 16px 0;
-    }
-`
-function PostAreaList() {
-    return (
-        <li>
-            <PostLink to="#">
-                <HomeImgPost />
-            </PostLink>
-        </li>
-    )
-}
-
 function MyProfilePage() {
     return (
         <>
@@ -68,19 +24,7 @@ function MyProfilePage() {
             <ProfileSection>
                 <UserProfile />
                 <UserProduct />
-                <PostArea>
-                    <div className='postAreaTop'>
-                        <PostShowBtns>
-                            <img className='postBtnImg' src={IconPostListOn} alt='포스트리스트형식' />
-                        </PostShowBtns>
-                        <PostShowBtns>
-                            <img className='postBtnImg' src={IconPostAlbumOff} alt='포스트앨범형식' />
-                        </PostShowBtns>
-                    </div>
-                    <PostAreaListUl>
-                        <PostAreaList />
-                    </PostAreaListUl>
-                </PostArea>
+                <UserPost />
             </ProfileSection>
             <TabMenu />
         </>
