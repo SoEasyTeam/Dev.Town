eee363e7aa452f9b082effb6cc0175e215e79f5f
chuhoon <khchu1220@naver.com>
2022-07-20 10:20:37 +0900

    HomePage Feed 리덕스 세팅 완료

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,16 +7,24 @@ import styled from 'styled-components'
 import { HomeNoFollower } from '../components/home/HomeNoFollower'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { homeFeedAction } from '../redux/actions/homeFeedAction'
 
 function HomePage() {
     const token = useSelector(state => state.auth.token);
+    const accountname = useSelector(state => state.auth.accountname);
     const dispatch = useDispatch();
+    const posts = useSelector(state=>state.homefeed.item);
+    console.log(posts);
+    
     useEffect(() => {
+        dispatch(homeFeedAction.homeFeed(token, accountname));
     },[])
     return (
         <>
             <TopMainNav />
-            <HomeNoFollower/>
+            {
+                posts.length === 0 ? <HomeNoFollower/> : <HomePost />
+            } 
             <TabMenu />
         </>
     )
