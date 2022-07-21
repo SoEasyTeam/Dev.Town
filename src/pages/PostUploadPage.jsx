6bb413f0f9a36bd5bf98a22217250ea99196bc41
daheejo <happyj6528@gmail.com>
2022-07-21 22:09:08 +0900

    Fix: 사진 최대 3장 미리보기 구현

--- src/pages/PostUploadPage.jsx
+++ src/pages/PostUploadPage.jsx
@@ -1,41 +1,43 @@
-import styled from 'styled-components'
-import { useState, useRef, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
+import styled from 'styled-components';
+import { useState, useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
-import {TopUploadNav} from '../components/common/TopNav'
-import { DefaultProfileImg, ImgUploadBtn,} from '../components/common/ProfileButtons'
-import { postAction } from '../redux/actions/postAction'
+import { TopUploadNav } from '../components/common/TopNav';
+import {
+    DefaultProfileImg,
+    ImgUploadBtn,
+} from '../components/common/ProfileButtons';
+import { postAction } from '../redux/actions/postAction';
 
 const UploadForm = styled.form`
     height: 100vh;
     display: flex;
     flex-direction: column;
-`
+`;
 
 const UploadSection = styled.section`
     display: flex;
     justify-content: center;
     gap: 20px;
     padding: 50px;
-
-`
+`;
 
 const UploadInputSection = styled.section`
     /* display: flex; */
     flex-direction: column;
-    
-    .uploadInput{
+
+    .uploadInput {
         width: 70vw;
         height: auto;
         border: none;
     }
 
-    .uploadInput::placeholder{
+    .uploadInput::placeholder {
         position: absolute;
-        top:0;
+        top: 0;
     }
-`
+`;
 
 const UploadImgInput = styled.input`
     position: absolute;
@@ -45,83 +47,116 @@ const UploadImgInput = styled.input`
     height: 1px;
     overflow: hidden;
     padding: 0;
-`
+`;
 
-function UploadPage(){
-    const history = useHistory()
-    const [postText, setPostText]=useState('')
-    const [uploadedImg, setUploadedImg] = useState('')
-    const [imgPreview, SetImgPreview] = useState('')
-    const fileInputRef = useRef()
-    const dispatch = useDispatch()
+function UploadPage() {
+    const history = useHistory();
+    const [postText, setPostText] = useState('');
+    const [uploadedImg, setUploadedImg] = useState('');
+    const [imgPreview, SetImgPreview] = useState([]);
+    const fileInputRef = useRef();
+    const dispatch = useDispatch();
 
-    const token = useSelector(state=>state.auth.token)
+    const token = useSelector((state) => state.auth.token);
 
-    const onSubmitHandler = (e)=>{
-        e.preventDefault()
-        if (uploadedImg == '' && postText == ''){
-            alert('게시물을 작성해주세요')
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        if (uploadedImg === '' && postText === '') {
+            alert('게시물을 작성해주세요');
         } else {
-            console.log('submit succeed')
-            history.push('/myprofile')
-            dispatch(postAction.post(token, uploadedImg, postText))
-
+            console.log('submit succeed');
+            history.push('/myprofile');
+            dispatch(postAction.post(token, uploadedImg, postText));
         }
-        
-    }
+    };
 
-    const HandlePostText = (e)=>{
-        setPostText(e.target.value)
-    }
-    useEffect(()=>{
-        if (uploadedImg){
-            const reader = new FileReader()
-            reader.onloadend=()=>{
-                SetImgPreview(reader.result)
-            }
-            reader.readAsDataURL(uploadedImg)
-        } else {
-            SetImgPreview(null)
+    const HandlePostText = (e) => {
+        setPostText(e.target.value);
+    };
+    useEffect(() => {
+        for (let index = 0; index < uploadedImg.length; index++) {
+            const file = uploadedImg[index];
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                SetImgPreview((prev) => {
+                    const newPreviews = [...prev, reader.result];
+                    if (newPreviews.length > 3){
+                        const newPreviewsSliced = newPreviews.slice(0,3)
+                        return newPreviewsSliced
+                    }
+                    return newPreviews;
+                });
+            };
+            reader.readAsDataURL(file);
         }
-    }, [uploadedImg])
+    }, [uploadedImg]);
 
-    
-    return(
+    return (
         <>
             <UploadForm onSubmit={onSubmitHandler}>
-                <TopUploadNav/>
+                <TopUploadNav />
                 <UploadSection>
                     <div className='profileImg'>
-                        <DefaultProfileImg/>
+                        <DefaultProfileImg />
                     </div>
                     <UploadInputSection>
-                        <textarea className='uploadInput' placeholder='게시글 입력하기...' rows={10} onChange={HandlePostText}/>
-                        {imgPreview && 
-                        <img className='uploaded-img' src={imgPreview} alt="게시물 이미지" style={{maxWidth: '500px', maxHeight:'500px'}}/>}
-                        <UploadImgInput 
-                        type="file"
-                        multiple 
-                        name="uploadedImg" id="uploadedImg" accept="image/*" 
-                        ref={fileInputRef}
-                        onChange={e=>{
-                            const file = e.target.files[0]
-                            if (file){
-                                setUploadedImg(file)
-                                console.log('image set!');
-                            } else {
-                                setUploadedImg(null)
-                            }
-                        }}
+                        <textarea
+                            className='uploadInput'
+                            placeholder='게시글 입력하기...'
+                            rows={10}
+                            onChange={HandlePostText}
                         />
-                        <ImgUploadBtn htmlFor="uploadedImg"onClick={e=>{
-                            e.preventDefault()
-                            fileInputRef.current.click()
-                        }} />
+                        {imgPreview &&
+                            imgPreview.map((imgUrl) => (
+                                <article>
+                                    <img
+                                        className='uploaded-img'
+                                        src={imgUrl}
+                                        alt='게시물 이미지'
+                                        style={{
+                                            maxWidth: '500px',
+                                            maxHeight: '500px',
+                                        }}
+                                    />
+                                </article>
+                            ))}
+                        <UploadImgInput
+                            type='file'
+                            multiple='multiple'
+                            name='uploadedImg'
+                            id='uploadedImg'
+                            accept='image/*'
+                            ref={fileInputRef}
+                            onChange={(e) => {
+                                // 파일리스트를 받는다
+                                // 파일리스트들을 각각 url로 만들어준다
+                                // 이미지 url들을 가지고있는 배열이 생긴다
+                                // 이 배열을 api명세에 맞게 문자열로 만든다.
+                                // Post업로드 요청을한다.
+
+                                const fileList = e.target.files;
+
+                                // const file = e.target.files[0];
+                                if (fileList.length > 0) {
+                                    setUploadedImg(fileList);
+                                    console.log('image set!');
+                                } else {
+                                    setUploadedImg(null);
+                                }
+                            }}
+                        />
+                        <ImgUploadBtn
+                            htmlFor='uploadedImg'
+                            onClick={(e) => {
+                                e.preventDefault();
+                                fileInputRef.current.click();
+                            }}
+                        />
                     </UploadInputSection>
                 </UploadSection>
             </UploadForm>
         </>
-    )
+    );
 }
 
-export default UploadPage
+export default UploadPage;
