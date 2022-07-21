1953b71fff91f0d7dda2421a536b3fc4f2dbe569
daheejo <happyj6528@gmail.com>
2022-07-21 09:30:13 +0900

    Test: fetch 오류

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authenticateReducer from './authenticateReducer';
 import joinReducer from './joinReducer';
+import postReducer from './postReducer';
 import profileReducer from './profileReducer';
 
 export default combineReducers({
     auth: authenticateReducer,
     join: joinReducer,
     profile: profileReducer,
+    post: postReducer,
 });
