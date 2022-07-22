5ebacbd3e1a79938024605281c756ecd5a5fd994
daheejo <happyj6528@gmail.com>
2022-07-22 11:04:14 +0900

    Test: uploadfiles 보일러플레이트 생성 중

---
+++ src/redux/actions/uploadFilesAction.js
@@ -0,0 +1,24 @@
+function upload(token, formData){
+    return async (dispatch, getState) =>{
+        let url = 'https://mandarin.api.weniv.co.kr';
+        const reqPath = `/image/uploadfiles`
+        try {
+            let res = await fetch(url+reqPath,{
+                method:'POST',
+                body: formData
+            })
+            const resJson = await res.json()
+            const postImgName = resJson['filename']
+            dispatch({
+                type:'FILE_UPLOAD_SUCCESS',
+                payload:{
+                    files: postImgName
+                }
+            })
+        } catch(error){
+            console.log(error);
+        }
+    }
+}
+
+export const uploadFilesAction = {upload}
---
+++ src/redux/reducers/uploadFilesReducer.js
@@ -0,0 +1,22 @@
+let initialState={
+    files: '',
+}
+
+function uploadFilesReducer(state=initialState, action){
+    let {type, payload} = action
+    switch(type){
+        case 'FILE_UPLOAD_SUCCESS':
+            console.log('파일전송성공!');
+            return {
+                ...state,
+                files: payload.files
+            }
+        default:
+            return {
+                ...state
+            }
+                
+    }
+}
+
+export default uploadFilesReducer
