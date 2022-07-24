4fdaa95ca836aae3b8be746a74a9fe489ddfdddf
daheejo <happyj6528@gmail.com>
2022-07-24 22:49:45 +0900

    Feat: CommentList 컴포넌트 추가

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,12 +1,14 @@
 import {TopBasicNav} from '../components/common/TopNav'
 import HomeImgPost from '../components/common/HomeImgPost'
-import Comment from '../components/common/Comment'
+import Comment from '../components/comment/Comment'
+import CommentList from '../components/comment/CommentList'
 
 function PostPage(){
     return(
         <>
         <TopBasicNav/>
         <HomeImgPost />
+        <CommentList/>
         <Comment/>
         </>
     )
