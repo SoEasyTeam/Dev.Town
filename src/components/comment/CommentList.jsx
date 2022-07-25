8a92c71aabbe5d560b116b3822185582dea9ef4b
daheejo <happyj6528@gmail.com>
2022-07-25 10:05:26 +0900

    Style: 댓글 컴포넌트 스타일링

--- src/components/comment/CommentList.jsx
+++ src/components/comment/CommentList.jsx
@@ -1,13 +1,61 @@
+import { useEffect } from "react"
 import styled from "styled-components"
+import { DefaultProfileImg } from "../common/ProfileButtons"
+import moreBtn from '../../assets/icon/icon-more-vertical.png'
 
-const CommentListBox = styled.div`
-    
+const CommentListSection = styled.section`
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 50px;
+    gap: 20px;
+    .profile-img {
+        width: 36px;
+
+    }
+`
+
+const CommentInfo = styled.div`
+    gap: 6px;
+    line-height: 40px;
+`
+
+const CommentFrom = styled.b`
+    font-size: 11px;
+    margin-right: 20px;
+`
+const CommentDate = styled.span`
+    font-size: 10px;
+    color: var(--subtitle-text);
+`
+
+const DetailOptionsBtn = styled.img`
+    src: ${moreBtn};
+    width: 20px;
+    margin-left: 140px;
+`
+const Comment = styled.p`
+    font-size: 14px;
 `
 
 function CommentList(){
+
+    useEffect(()=>{},)
     return (
-        <CommentListBox>
-        </CommentListBox>
+        <CommentListSection>
+            <div className="profile-img">
+            <DefaultProfileImg/>
+            </div>
+            <div className="comment-box">
+            <CommentInfo>
+                <CommentFrom>서귀포시 농장 주인</CommentFrom>
+                <CommentDate>2022.07.24</CommentDate>
+                <DetailOptionsBtn src={moreBtn}/>
+            </CommentInfo>
+            <Comment>방가방가</Comment>
+            </div>
+        </CommentListSection>
 
     )
 }
