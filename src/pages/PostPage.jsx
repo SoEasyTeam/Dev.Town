6bc42338cd67ec9ce80940fb8dc5774cb187ca03
Dayhun <lanna05@naver.com>
2022-07-25 16:11:44 +0900

    Style: postpage alert모달 추가

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,22 +1,34 @@
-import {TopBasicNav} from '../components/common/TopNav'
+import { useState } from 'react'
+import { TopBasicNav } from '../components/common/TopNav'
 import HomeImgPost from '../components/common/HomeImgPost'
 import Comment from '../components/common/Comment'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux';
+import { AlertLogoutModal } from '../components/common/AlertModal'
 
-function PostPage(){
+function PostPage() {
     const { id } = useParams();
-    const postViewId = useSelector(state=>state.homefeed.item).map((i) => i.id);
+    const postViewId = useSelector(state => state.homefeed.item).map((i) => i.id);
     console.log(postViewId);
     console.log(id);
     const postId = postViewId.filter(i => i === id);
     console.log(postId);
+    // alert 모달창
+    const [alertOn, setAlertOn] = useState(false);
+    function alertOnModal() {
+        setAlertOn(true);
+    }
+    function alertOffModal() {
+        document.body.style.overflow = "unset";
+        setAlertOn(false);
+    }
 
-    return(
+    return (
         <>
-            <TopBasicNav/>
+            <TopBasicNav alertOnModal={alertOnModal} />
             <HomeImgPost />
-            <Comment/>
+            <Comment />
+            {alertOn === true ? <AlertLogoutModal alertOffModal={alertOffModal} /> : ''}
         </>
     )
 }
