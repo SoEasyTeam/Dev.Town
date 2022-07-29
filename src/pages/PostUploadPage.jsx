60a55a6062b73bd1e3c25ddd4a1e2842ead5e419
Dayhun <lanna05@naver.com>
2022-07-29 15:38:19 +0900

    Refactor: post페이지 component 분리

--- src/pages/PostUploadPage.jsx
+++ src/pages/PostUploadPage.jsx
@@ -1,68 +1,10 @@
-import styled from 'styled-components';
-import { useState, useRef, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { React, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
-import { TopUploadNav } from '../components/common/nav';
-import {
-    DefaultProfileImg,
-    ImgUploadBtn,
-} from '../components/common/ProfileButtons';
-
 import { postAction } from '../redux/actions/postAction';
-
-const UploadForm = styled.form`
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-`;
-
-const UploadSection = styled.section`
-    display: flex;
-    justify-content: center;
-    gap: 20px;
-    padding: 50px;
-`;
-
-const UploadInputSection = styled.section`
-    /* display: flex; */
-    flex-direction: column;
-
-    .uploadInput {
-        width: 70vw;
-        height: auto;
-        border: none;
-    }
-
-    .uploadInput::placeholder {
-        position: absolute;
-        top: 0;
-    }
-
-    .preview-area {
-        display: flex;
-        gap: 5px;
-        overflow-x: auto;
-        height: 171px;
-    }
-    .preview-area .preview-item {
-        border-radius: 10px;
-        width: 168px;
-        height: 171px;
-        object-fit: cover;
-        max-height: 126px;
-    }
-`;
-
-const UploadImgInput = styled.input`
-    position: absolute;
-    left: -10000px;
-    top: auto;
-    width: 1px;
-    height: 1px;
-    overflow: hidden;
-    padding: 0;
-`;
+import { useHistory } from 'react-router-dom';
+import { TopUploadNav } from '../components/common/nav';
+import { DefaultProfileImg, ImgUploadBtn } from '../components/common/ProfileButtons';
+import { UploadForm, UploadSection, UploadInputSection, UploadImgInput } from '../components/post/index.style'
 
 function UploadPage() {
     const history = useHistory();
@@ -71,7 +13,6 @@ function UploadPage() {
     const [imgPreview, setImgPreview] = useState([]);
     const fileInputRef = useRef();
     const dispatch = useDispatch();
-
     const token = useSelector((state) => state.auth.token);
     const imgUrl = useSelector((state) => state.post.img);
 
@@ -120,24 +61,6 @@ function UploadPage() {
             }
         });
     };
-    // useEffect(() => {
-    //     for (let index = 0; index < uploadedImg.length; index++) {
-    //         const file = uploadedImg[index];
-    //         const reader = new FileReader();
-    //         reader.onloadend = () => {
-    //             SetImgPreview((prev) => {
-    //                 let newPreviews = [...prev, reader.result];
-    //                 if (newPreviews.length > 3) {
-    //                     alert('이미지는 최대 3장까지 업로드가 가능합니다.');
-    //                     newPreviews = newPreviews.slice(0, 3);
-    //                     return newPreviews;
-    //                 }
-    //                 return newPreviews;
-    //             });
-    //         };
-    //         reader.readAsDataURL(file);
-    //     }
-    // }, [uploadedImg]);
 
     return (
         <>
