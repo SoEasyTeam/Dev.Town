cb928652d4353f2db187f63d38d3ac0637330433
daheejo <happyj6528@gmail.com>
2022-07-29 14:45:53 +0900

    Refactor : 404페이지 컴포넌트 분리

---
+++ src/components/error/index.style.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+import ErrorImg from '../../assets/error404-icon.png'
+
+const ErrorPageBox=styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const ErrorIcon = styled.img.attrs({
+    src: `${ErrorImg}`,
+    alt: '404 에러'
+})`
+    width: 148px;
+`
+
+const ErrorMessage=styled.p`
+    margin:25px 0;
+    font-size: 14px;
+    color: var(--subtitle-text);
+`
+
+export { ErrorIcon, ErrorPageBox, ErrorMessage }
