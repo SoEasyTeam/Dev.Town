19c378f5ff471e2f2ddfe8e3c0c560432967f1d2
Dayhun <lanna05@naver.com>
2022-07-31 16:11:07 +0900

    Style: 상대프로필페이지에서 상대게시물 신고하기클릭시 alert

--- src/components/profile/userPost/index.jsx
+++ src/components/profile/userPost/index.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useLocation } from "react-router-dom";
 import HomeImgPost from '../../common/HomeImgPost';
 import { AlertPostModal, AlertDeclareModal } from '../../common/alert';
 import { PostListBtns, PostAlbumBtns, PostArea, PostAreaListUl } from './index.style';
@@ -48,6 +48,9 @@ function UserPost(props, id) {
     const [userPostData, setUserPostData] = useState('')
     const [alertOn, setAlertOn] = useState(false);
     const [isActive, setIsActive] = useState(true);
+    const location = useLocation();
+
+
     // console.log('버튼', isActive)
     const getData = async (account) => {
         const res = await fetch(`https://mandarin.api.weniv.co.kr/post/${account}/userpost`, {
@@ -75,6 +78,8 @@ function UserPost(props, id) {
     }
     function alertOnModal() {
         setAlertOn(true);
+        console.log('게시물 계정이름', id);
+        console.log('로그인 유저 계정이름', accountname);
     }
     function alertOffModal() {
         document.body.style.overflow = "unset";
@@ -95,8 +100,8 @@ function UserPost(props, id) {
                     <PostAreaList userPostData={userPostData} alertOnModal={alertOnModal} />
                 </PostAreaListUl>
             </PostArea>
-            {alertOn === true && id !== accountname ? <AlertPostModal alertOffModal={alertOffModal} /> : ''}
-            {alertOn === true && id === accountname ? <AlertDeclareModal alertOffModal={alertOffModal} /> : ''}
+            {alertOn === true && location.pathname.split("/")[1] === "myprofile" ? <AlertPostModal alertOffModal={alertOffModal} /> : ''}
+            {alertOn === true && location.pathname.split("/")[1] === "yourpage" ? <AlertDeclareModal alertOffModal={alertOffModal} /> : ''}
         </>
     )
 }
