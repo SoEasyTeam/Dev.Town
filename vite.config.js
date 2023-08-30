12071635220e5cda44d5f28e1345df079336a79d
chuhoon <khchu1220@naver.com>
2023-08-30 12:32:16 +0900

    Refactoring : Absolute path initialization

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
     plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+    resolve: {
+        alias: [
+            { find: '@', replacement: '/src' },
+            { find: '@components', replacement: '/src/components' },
+            { find: '@constants', replacement: '/src/constants' },
+            { find: '@hooks', replacement: '/src/hooks' },
+            { find: '@pages', replacement: '/src/pages' },
+            { find: '@redux', replacement: '/src/redux' },
+            { find: '@utils', replacement: '/src/utils' },
+        ],
+    },
     server: {
         port: 3000,
     },
