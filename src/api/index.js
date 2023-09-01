f04f8518d03e69d6dfb97e00836a182f8889ba77
chuhoon <khchu1220@naver.com>
2023-09-01 20:20:59 +0900

    Refactor : Create a common Axios Func

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const baseURL = import.meta.env.VITE_REACT_APP_API_URL;
+const token = localStorage.getItem('token');
+
+export const customAxios = async (type, parameter, endpoint) => {
+    const { data } = await axios({
+        method: type,
+        url: baseURL + endpoint,
+        data: parameter,
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    return data;
+};
